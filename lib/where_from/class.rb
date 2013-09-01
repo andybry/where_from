905ad460eaab85e503a2d3c7b3c86f81cd334452
Andy Bryant <ar.bryant@btinternet.com>
2013-09-01 14:43:21 +0100

    Class#superclasses_with_method
    Add a method to Class that finds all the superclasses
    that contain a given method

--- lib/where_from/class.rb
+++ lib/where_from/class.rb
@@ -11,4 +11,15 @@ class Class
     end
   end
 
+
+  #
+  # 
+  #
+  def superclasses_with_method(method_symbol)
+    all_superclasses = superclasses
+    all_superclasses.select do |klass|
+      klass.method_defined? method_symbol
+    end
+  end
+
 end
--- spec/where_from/class_spec.rb
+++ spec/where_from/class_spec.rb
@@ -3,8 +3,9 @@ describe Class do
   describe "#superclasses" do
 
     it "exists as a method on Class objects" do
-      class MyClass1; end
-      expect(MyClass1.respond_to? :superclasses).to be_true
+      class MyClass; end
+      expect(MyClass.respond_to? :superclasses).to be_true
+      Object.send :remove_const, :MyClass
     end
 
     context "when called on BasicObject" do
@@ -21,23 +22,66 @@ describe Class do
 
     context "when called on a class <Klass> inheriting from Object" do
       it "returns [<Klass>, Object, BasicObject]" do
-        class MyClass2; end
-        expect(MyClass2.superclasses).to eq([MyClass2, Object, BasicObject])
+        class MyClass; end
+        expect(MyClass.superclasses).to eq([MyClass, Object, BasicObject])
+        Object.send :remove_const, :MyClass
       end
     end
 
     context "when called on a class declared to inherit from another" do
       it "returns [<Klass>, <flattened parent class chain>]" do
-        class MyClass3; end
-        class MyClass4 < MyClass3; end
-        expect(MyClass4.superclasses).to eq([MyClass4, *MyClass3.superclasses])
+        class MyClassA; end
+        class MyClassB < MyClassA; end
+        expect(MyClassB.superclasses).to eq(
+          [MyClassB, *MyClassA.superclasses]
+        )
+        Object.send :remove_const, :MyClassB
+        Object.send :remove_const, :MyClassA
       end
     end
 
-  end # end of superclass method definition
+  end # end of superclasses method definition
 
   
-  describe "#ancestors_with_method" do
-  end
+  describe "#superclasses_with_method" do
+    it "exists as a method on Class objects" do
+      class MyClass; end
+      expect(MyClass.respond_to? :superclasses_with_method).to be(true)
+        Object.send :remove_const, :MyClass
+    end
 
+    context "when called on a class without a method" do
+      it "returns an empty array" do
+        class MyClass; end
+        expect(
+          MyClass.superclasses_with_method :undefined_method_name
+        ).to eq([])
+        Object.send :remove_const, :MyClass
+      end
+    end
+
+    context "when called on a class which defines a method" do
+      it "returns a 1 element array containing the class" do
+        class MyClass; def meth1; end; end
+        expect(MyClass.superclasses_with_method :meth1).to eq([MyClass])
+        Object.send :remove_const, :MyClass
+      end
+    end
+
+    context "when called on a class which inherits a method" do
+      it "returns an array of all superclasses which define or inherit the method" do
+        class MyClassA; def meth1; end; end
+        class MyClassB < MyClassA; end
+        class MyClassC < MyClassB; end;
+        expect(
+          MyClassC.superclasses_with_method :meth1
+        ).to eq([MyClassC, MyClassB, MyClassA])
+        Object.send :remove_const, :MyClassC
+        Object.send :remove_const, :MyClassB
+        Object.send :remove_const, :MyClassA
+      end
+    end
+
+  end # end of superclasses_with_method method definition
+  
 end
